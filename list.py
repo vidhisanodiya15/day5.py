29d51718cd8eaeebb76a2f6879515eff3284e34e
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-03 22:14:05 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+marks =[3,4,5,"vidhi",True]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+
+# print(marks[-3]) #negative index
+# print(marks[len(marks)-3]) #positive
+# print(marks[5-3])  #positive index
+# print(marks[2]) #pos index
+
+# if 7 in marks:
+#     print("Yes")
+# else:
+#     print("No")    
+    
+# if "idhi" in "vidhi":
+#     print("Yes")
+
+print(marks)
+print(marks[1:-1])
+    
